4cc2eb400a7ba8bf61a52e71b5a3d4deac92860d
Elen Severina <an.egorova.mail@gmail.com>
2021-12-05 13:38:07 +0200

    interval commit

--- interval.js
+++ interval.js
@@ -9,21 +9,63 @@
 
 
 
-const array = [1, 2, 3, 'Hello', {day: 'Wednesday'}, 5, 6];
-let delay = 3000;
+// const array = [1, 2, 3, 'Hello', {day: 'Wednesday'}, 5, 6];
+// let delay = 3000;
 
-function asyncRecursive(array, timeout) {
-    timeout = delay;
+// function asyncRecursive(array, timeout) {
+//     timeout = delay;
 
-    for(let i = 0; i < array.length; i++) {
-        setTimeout(function () {
-            console.log(array[i]);
-        }, i * timeout);
-    }
+//     for(let i = 0; i < array.length; i++) {
+//         setTimeout(function () {
+//             console.log(array[i]);
+//         }, i * timeout);
+//     }
+// }
+
+// setTimeout(asyncRecursive, 3000, array);
+
+
+
+
+function printArray(array, timeout) {
+  for (var i = 0; i < array.length; i += 1) {//и поменять тут
+      //добавлять код тут
+      setTimeout(() => {
+          console.log(array[i]);
+      }, timeout);
+      // и добавлять код тут
+  }
 }
 
-setTimeout(asyncRecursive, 3000, array);
+printArray([1, 2, 3], 1000);
+
+// разобраться почему 3 раза выводится undefined
+// 2 способа как поправить код, так что-бы элементы выводились правильно
+// замыкания, var/let/const какая между ними разница
 
 
+//1)
+function printArray(array, timeout) {
+  for (var i = 0; i < array.length; i += 1) {
+    (function(i) {
+      setTimeout(() => {
+          console.log(array[i]);
+      }, array[i] * timeout)
+    })(array[i]);
+  }
+}
 
-    
+printArray([1, 2, 3], 3000);
+
+//2
+function printArray(array, timeout) {
+  for (let i = 0; i < array.length; i += 1) {
+    setTimeout(() => {
+        console.log(array[i]);
+    }, array[i] * timeout)
+  }
+}
+
+printArray([1, 2, 3], 3000);
+
+
